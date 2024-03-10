600a4eeb3ce1ab8f228659b48bf576d9aa1b5cd9
a1897914 <140685907+a1897914@users.noreply.github.com>
2024-03-10 20:14:26 +1030

    Add files via upload

--- Referee.cpp
+++ Referee.cpp
@@ -8,14 +8,14 @@ Player* Referee::refGame(Player* player1, Player* player2){
 Move* p1_move = player1->makeMove();
 Move* p2_move = player2->makeMove();
 
-for(int i=0; i < p2_move -> getWinner().size(); i++ ){
-if(p1_move -> getName() == p2_move -> getWinner().at(i)){
+for(int size_t=0; size_t < p2_move -> getWinner().size(); size_t++ ){
+if(p1_move -> getName() == p2_move -> getWinner().at(size_t)){
 return player2;
 }
 }
 
-for(int i=0; i < p2_move -> getLoser().size(); i++ ){
-if(p1_move -> getName() == p2_move -> getLoser().at(i)){
+for(int size_t=0; size_t < p2_move -> getLoser().size(); size_t++ ){
+if(p1_move -> getName() == p2_move -> getLoser().at(size_t)){
 return player1;
 }
 }
